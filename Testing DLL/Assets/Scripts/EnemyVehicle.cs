ce96d0a045475182c2d663e5121ffc83290bc49e
Coehl Gleckner (RIT Student) <crg5646@ad.rit.edu>
2019-04-14 15:28:35 -0400

    -Starting work on enemy vehicles
    -Will be received from the server information

---
+++ Testing DLL/Assets/Scripts/EnemyVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVehicle : MonoBehaviour
+{
+    // attributes
+    public int playerID;
+    public Vector3 position;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = position;
+    }
+}
