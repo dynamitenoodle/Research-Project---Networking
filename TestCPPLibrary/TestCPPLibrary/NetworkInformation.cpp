0d8aa3830de14b39634e74085539ac04849839d4
Coehl Gleckner (RIT Student) <crg5646@ad.rit.edu>
2019-04-14 14:15:31 -0400

    Pushing stuff
    -Working to create namespace variables
    -Almost all working, just need to fix a SOCKET issue, running to papa egert

---
+++ TestCPPLibrary/TestCPPLibrary/NetworkInformation.cpp
@@ -0,0 +1,4 @@
+#include "stdafx.h"
+#include "NetworkInformation.h"
+
+// Information that we need saved
--- TestCPPLibrary/TestCPPLibrary/TestCPPLibrary.cpp
+++ TestCPPLibrary/TestCPPLibrary/TestCPPLibrary.cpp
@@ -3,58 +3,61 @@
 #pragma once
 #include "TestCPPLibrary.h"
 #include "stdafx.h"
+#include "NetworkInformation.h"
 
 using namespace std;
+using namespace NetworkInfo;
 extern "C" 
 {
 	bool __declspec(dllexport) Connect(int ip0, int ip1, int ip2, int ip3, int port)
 	{
 		bool connectionSuccess = false;
-		ofstream myfile;
-		myfile.open("DLL_Debug.txt");
-		myfile << "Started DLL Debug! " << endl;
+		myInfo.myfile.open("DLL_Debug.txt");
+		myInfo.myfile << "Started DLL Debug! " << endl;
 
 		// Startup Winsock aaa
-		myfile << "Setting up IP!." << endl;
+		myInfo.myfile << "Setting up IP!." << endl;
 
 		// sets up the IP with input
 		//string myIp = "129.21.178.67";
-		int ipNum;
-		string ip = "";
+		//int ipNum;
+		myInfo.ip = "";
 		
-		ip += to_string(ip0) + ".";
-		ip += to_string(ip1) + ".";
-		ip += to_string(ip2) + ".";
-		ip += to_string(ip3);
+		myInfo.ip += to_string(ip0) + ".";
+		myInfo.ip += to_string(ip1) + ".";
+		myInfo.ip += to_string(ip2) + ".";
+		myInfo.ip += to_string(ip3);
 
 		// Create a hint structure for the server
-		sockaddr_in serverHint;
-		serverHint.sin_family = AF_INET;
-		serverHint.sin_port = htons(port);
-		myfile << "Hint structure set up!" << endl;
+		myInfo.serverHint.sin_family = AF_INET;
+		myInfo.serverHint.sin_port = htons(port);
+		myInfo.myfile << "Hint structure set up!" << endl;
 
 		//myfile << "IP: " << ip << "      PORT: " << port << "/n"; aaa
-		myfile << "Setup IP" << endl;
-		myfile << "IP: " << ip << endl;
-		myfile << "PORT: " << port << endl;
+		myInfo.myfile << "Setup IP" << endl;
+		myInfo.myfile << "IP: " << myInfo.ip << endl;
+		myInfo.myfile << "PORT: " << port << endl;
 
 		// converts from string to binary for address
-		inet_pton(AF_INET, ip.c_str(), &serverHint.sin_addr); // address is of the server
-		int serverLength = sizeof(serverHint);
+		inet_pton(AF_INET, myInfo.ip.c_str(), &myInfo.serverHint.sin_addr); // address is of the server
+		myInfo.serverLength = sizeof(myInfo.serverHint);
 
+		myInfo.myfile << "Creating the socket" << endl;
 		// Socket creation
-		SOCKET serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+		SOCKET serverSocket;
+		serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+		myInfo.myfile << "Setting the myInfo serverSocket variable" << endl;
+		myInfo.serverSocket = serverSocket;
 
 		// Getting the serverIp
-		char serverIp[256];
-		ZeroMemory(serverIp, 256);
+		ZeroMemory(myInfo.serverIP, 256);
 
-		inet_ntop(AF_INET, &serverHint.sin_addr, serverIp, 256);
-		myfile << "Server IP (sin_addr): " << &serverIp << endl;
+		myInfo.myfile << "Creating the Server IP" << endl;
+		inet_ntop(AF_INET, &(myInfo.serverHint.sin_addr), myInfo.serverIP, 256);
+		myInfo.myfile << "Server IP (sin_addr): " << &myInfo.serverIP << endl;
 
 		// Preparing the status for the server
-		status serverStatus;
-		ZeroMemory((char*)&serverStatus, 128);
+		ZeroMemory((char*)&myInfo.serverStatus, 128);
 
 		// preparing the connect command
 		command connect;
@@ -62,40 +65,40 @@ extern "C"
 		connect.cmd = 'c';
 
 		// Test the connection to the server
-		myfile << "Preparing the test connection." << endl;
-		myfile << "Testing connection to server, if the next message does not appear please restart the program." << endl;
-		int testSend = sendto(serverSocket, (char*)&connect, 128, 0, (sockaddr*)&serverHint, serverLength);
-		myfile << "testSend value: " << testSend << endl;
+		myInfo.myfile << "Preparing the test connection." << endl;
+		myInfo.myfile << "Testing connection to server, if the next message does not appear please restart the program." << endl;
+		int testSend = sendto(myInfo.serverSocket, (char*)&connect, 128, 0, (sockaddr*)&myInfo.serverHint, myInfo.serverLength);
+		myInfo.myfile << "testSend value: " << testSend << endl;
 		if (testSend == SOCKET_ERROR)
 		{
 			//cout << "Sending the test message didn't work : " << WSAGetLastError() << endl;
-			myfile << "Failed the test connection." << endl;
-			myfile.close();
+			myInfo.myfile << "Failed the test connection." << endl;
+			myInfo.myfile.close();
 			return connectionSuccess;
 		}
 		else
 		{
-			myfile << "Succeded the test connection." << endl;
+			myInfo.myfile << "Succeded the test connection." << endl;
 		}
 
 		// Wait for a response from the server
-		myfile << "Preparing to receive from server connection." << endl;
-		int bytesInTest = recvfrom(serverSocket, (char*)&serverStatus, 128, 0, (sockaddr*)&serverHint, &serverLength);
+		myInfo.myfile << "Preparing to receive from server connection." << endl;
+		int bytesInTest = recvfrom(myInfo.serverSocket, (char*)&myInfo.serverStatus, 128, 0, (sockaddr*)&myInfo.serverHint, &myInfo.serverLength);
 		if (bytesInTest == SOCKET_ERROR)
 		{
 			//cout << "Error receiving from client " << WSAGetLastError() << endl;
-			myfile << "Did not receive from the server." << endl;
-			myfile.close();
+			myInfo.myfile << "Did not receive from the server." << endl;
+			myInfo.myfile.close();
 			return connectionSuccess;
 		}
 		else
 		{
-			myfile << "We got word from the server!" << endl;
+			myInfo.myfile << "We got word from the server!" << endl;
 			connectionSuccess = true;
 		}
 
-		myfile << "Closed the file!" << endl;
-		myfile.close();
+		myInfo.myfile << "Closed the file!" << endl;
+		myInfo.myfile.close();
 		return connectionSuccess;
 	}
 
