e537f06a415fcfa2dae0f32799211c6df2e5bd71
Coehl Gleckner (RIT Student) <crg5646@ad.rit.edu>
2019-04-14 12:43:10 -0400

    Pushing all them files
    -Re-updated all assets with modern version

---
+++ NetworkManager/Server/Player.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "pch.h"
+
+class Player
+{
+public:
+	Player();
+	Player(char clientIp[256], int playerNum, string name, Vector2 posOrg, sockaddr_in sock);
+	~Player();
+
+	// Gets the player's name
+	string GetName();
+
+	sockaddr_in sockaddr_in;
+	// Gets the player's IP Address
+	string GetIp();
+
+	// Gets the player's Num
+	int GetNum();
+
+	Vector2 position;
+
+private:
+	char clientIp[256];
+	int playerNum;
+	string name;
+
+};
+class ClientManager
+{
+public:
+	static int playerCount;
+	static Player* playerArray;
+	ClientManager();
+	~ClientManager();
+
+private:
+
+};
+
+ClientManager::ClientManager()
+{
+}
+
+ClientManager::~ClientManager()
+{
+}
---
+++ NetworkManager/Server/Status.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "pch.h"
+
+class Status
+{
+public:
+	Status();
+	~Status();
+
+	status connect;
+};
+
